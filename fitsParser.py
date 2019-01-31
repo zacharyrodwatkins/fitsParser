9b6a4a6975ae1ec103d9f79279f055dce2a30d78
Zack <zackwatkins604@gmail.com>
2019-01-30 17:12:30 -0800

    comments added

--- fitsParser.py
+++ fitsParser.py
@@ -23,6 +23,7 @@ NUM_REGEX = re.compile("[0-9]+")
 IN_REGEX = re.compile(".+-i")
 OUT_REGEX = re.compile(".+-o")
 INCLUDE_ALL = re.compile("\s*-all\s*$|\n")
+COMMENT_REGEX = re.compile(".*#.*")
 
 class fitsParser:
 
@@ -88,35 +89,41 @@ class fitsParser:
         else:
             #TODO: account for x as y //done 
             for name in include:
-                In, Out = False, False
-                
-                if bool(INCLUDE_ALL.match(name)):
-                    self.includeAll = True
-                
-                else:
-                    if bool(IN_REGEX.match(name)):
-                        name = re.sub('\s*-i\s*', "", name)
-                        In = True
+            
+                if bool(COMMENT_REGEX.match(name)):
+                    name = fitsParser.findAndRemove(name, COMMENT_REGEX, re.compile("#.*"))
+                    
+                if name!='':                                  
+            
+                    In, Out = False, False
 
-                    elif bool(OUT_REGEX.match(name)):
-                        name = re.sub('\s*-o\s*',"", name)
-                        Out = True
+                    if bool(INCLUDE_ALL.match(name)):
+                        self.includeAll = True
 
-                    if bool(AS_REGEX.match(name)):
-                        realName = INCLUDE_NAME_REGEX.findall(name)[0]
-                        alias = ALIAS_REGEX.findall(name)[0]
+                    else:
+                        if bool(IN_REGEX.match(name)):
+                            name = re.sub('\s*-i\s*', "", name)
+                            In = True
 
-                    else: 
-                        alias = name
-                        realName = name
+                        elif bool(OUT_REGEX.match(name)):
+                            name = re.sub('\s*-o\s*',"", name)
+                            Out = True
 
-                    if bool(NUM_REGEX.match(realName)):
-                        realName = names[int(realName)-1]
+                        if bool(AS_REGEX.match(name)):
+                            realName = INCLUDE_NAME_REGEX.findall(name)[0]
+                            alias = ALIAS_REGEX.findall(name)[0]
 
-                    if In:
-                        self.inputs.append(alias)
-                    elif Out:
-                        self.outputs.append(alias)
+                        else: 
+                            alias = name
+                            realName = name
+
+                        if bool(NUM_REGEX.match(realName)):
+                            realName = names[int(realName)-1]
+
+                        if In:
+                            self.inputs.append(alias)
+                        elif Out:
+                            self.outputs.append(alias)
 
                     params[realName] = {'format': formats[names.index(str(realName))], 'units': units[unitNumbs.index(names.index(str(realName)))] if names.index(str(realName)) in unitNumbs else None, 'alias': alias}
                         
